3bccd1ca88bc9a7725a24138084de08c7c97cbcb
hadyaraves <hadyaraves@gmail.com>
2016-11-29 12:37:16 +0700

    Update safelinkjs.js

--- safelinkjs.js
+++ safelinkjs.js
@@ -24,5 +24,6 @@ var a_to_vj=document.getElementById("anonyminized");var a_to_vk=document.getElem
 if(a_to_vk)
 {a_to_vk.innerHTML+=a_to_va;}}
 function a_to_fa()
-{var a_to_vf=new Array();protected_links=protected_links.replace("nzmdownload.blogspot.com,youtube.com,facebook.com,google.com","");a_to_vf=protected_links.split(",");return a_to_vf;}
+{var a_to_vf=new Array();protected_links=protected_links.replace(" ","");a_to_vf=protected_links.split(",");return a_to_vf;}
+protected_links = "nzmdownload.blogspot.com,youtube.com,facebook.com,google.com";
 auto_safelink();
