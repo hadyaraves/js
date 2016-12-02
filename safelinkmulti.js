33d8c4e8b628ad5cae543357a4ec41e9bc26daa7
hadyaraves <hadyaraves@gmail.com>
2016-12-02 23:35:09 +0700

    Update safelinkmulti.js

--- safelinkmulti.js
+++ safelinkmulti.js
@@ -19,7 +19,7 @@ var a_to_ve="";var a_to_vf=new Array();var a_to_vg=0;a_to_ve=document.getElement
 j++;}
 if(a_to_vh==false)
 {var encryptedUrl=Base64.encode(a_to_vi);
-var x=Math.floor((Math.random()*5)+ 1);
+var x=Math.floor((Math.random()*1)+ 1);
 var xxx=null;
 if(x=="1"){xxx="stress-eating-helps-when-they.html"}a_to_ve[i].href="https://nzmsafe.blogspot.com/p/"+xxx+"?url="+ encryptedUrl;a_to_ve[i].rel="nofollow";a_to_vb++;a_to_vc+=i+":::"+ a_to_ve[i].href+"\n";}}
 var a_to_vj=document.getElementById("anonyminized");var a_to_vk=document.getElementById("found_links");if(a_to_vj)
