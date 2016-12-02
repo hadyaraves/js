12f74067fc9f66bc7c19c6c38af029dedef09903
hadyaraves <hadyaraves@gmail.com>
2016-12-02 23:21:00 +0700

    Update safelinkmulti.js

--- safelinkmulti.js
+++ safelinkmulti.js
@@ -21,11 +21,7 @@ if(a_to_vh==false)
 {var encryptedUrl=Base64.encode(a_to_vi);
 var x=Math.floor((Math.random()*5)+ 1);
 var xxx=null;
-if(x=="1"){xxx="stress-eating-helps-when-they.html"}
-if(x=="2"){xxx="google-drive-cloud.html"}
-if(x=="3"){xxx="how-to-read-forex-chart-properly.html"}
-if(x=="4"){xxx="auto-nsurance-quoe-2015.html"}
-if(x=="5"){xxx="ways-to-get-cheapest-healthinsurance.html"}a_to_ve[i].href="https://nzmsafe.blogspot.com/p/"+xxx+"?url="+ encryptedUrl;a_to_ve[i].rel="nofollow";a_to_vb++;a_to_vc+=i+":::"+ a_to_ve[i].href+"\n";}}
+if(x=="1"){xxx="stress-eating-helps-when-they.html"}a_to_ve[i].href="https://nzmsafe.blogspot.com/p/"+xxx+"?url="+ encryptedUrl;a_to_ve[i].rel="nofollow";a_to_vb++;a_to_vc+=i+":::"+ a_to_ve[i].href+"\n";}}
 var a_to_vj=document.getElementById("anonyminized");var a_to_vk=document.getElementById("found_links");if(a_to_vj)
 {a_to_vj.innerHTML+=a_to_vb;}
 if(a_to_vk)
